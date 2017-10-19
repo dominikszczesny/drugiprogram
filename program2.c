98bc28b6dcaf4cf02f7ba18b2c8f6ea1bc273104
dominikszczesny <dominikszczesny5@wp.pl>
2017-10-19 17:48:16 +0200

    drugi program

---
+++ program2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+
+int main() 
+{
+
+		putchar('v'); /* wypisz znak*/
+		putchar('\n');/*Xznak konca lini*/ 
+		puts("wypisz string / ciag znakow");
+ 		puts("\nAla ma kota,\na kot ma Ale");
+                return 0;
+
+
+
+}
+
+
+
+
+
+
